f4ddad5799336d8c0034cf7c460d021bffba718d
zlsu <645358999@qq.com>
2016-03-12 15:58:25 +0800

    1

--- src/com/zlsu/Wh.java
+++ src/com/zlsu/Wh.java
@@ -7,8 +7,11 @@ import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
 public class Wh {
-	
 
 	/**
 	 * @param urlAll
@@ -18,39 +21,50 @@ public class Wh {
 	 * @return 返回结果
 	 */
 	public static String request(String httpUrl, String httpArg) {
-	    BufferedReader reader = null;
-	    String result = null;
-	    StringBuffer sbf = new StringBuffer();
-	    httpUrl = httpUrl + "?" + httpArg;
+		BufferedReader reader = null;
+		String result = null;
+		StringBuffer sbf = new StringBuffer();
+		httpUrl = httpUrl + "?" + httpArg;
 
-	    try {
-	        URL url = new URL(httpUrl);
-	        HttpURLConnection connection = (HttpURLConnection) url
-	                .openConnection();
-	        connection.setRequestMethod("GET");
-	        // 填入apikey到HTTP header
-	        connection.setRequestProperty("apikey",  "fbcbd02ce99286ab00848dedcfe5846c");
-	        connection.connect();
-	        InputStream is = connection.getInputStream();
-	        reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-	        String strRead = null;
-	        while ((strRead = reader.readLine()) != null) {
-	            sbf.append(strRead);
-	            sbf.append("\r\n");
-	        }
-	        reader.close();
-	        result = sbf.toString();
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	    return result;
+		try {
+			URL url = new URL(httpUrl);
+			HttpURLConnection connection = (HttpURLConnection) url
+					.openConnection();
+			connection.setRequestMethod("GET");
+			// 填入apikey到HTTP header
+			connection.setRequestProperty("apikey",
+					"fbcbd02ce99286ab00848dedcfe5846c");
+			connection.connect();
+			InputStream is = connection.getInputStream();
+			reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			String strRead = null;
+			while ((strRead = reader.readLine()) != null) {
+				sbf.append(strRead);
+				sbf.append("\r\n");
+			}
+			reader.close();
+			result = sbf.toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
-	public static void main(String [] args) throws UnsupportedEncodingException{
+	public static void main(String[] args) throws UnsupportedEncodingException {
 		String httpUrl = "http://apis.baidu.com/apistore/weatherservice/recentweathers";
-		String httpArg = "cityid=101040200";
+		String httpArg = "cityname=长乐";
 		String jsonResult = request(httpUrl, httpArg);
-		System.out.println(jsonResult+"\u6c38\u5ddd");
+		JSONObject jsonObject = JSON.parseObject(jsonResult);
+		jsonObject = jsonObject.getJSONObject("retData");
+		JSONObject jsonObject1 = jsonObject.getJSONObject("today");
+		JSONArray list = jsonObject.getJSONArray("forecast");
+		System.out.println(jsonObject1);
+		JSONObject jsonObject2 = JSONObject.parseObject(list.get(0).toString());
+		System.out.println(jsonObject2);
+
+		System.out.println(jsonResult);
+		// System.out.println(jsonObject);
+		System.out.println(jsonResult + "\u6c38\u5ddd");
 	}
 
 }
